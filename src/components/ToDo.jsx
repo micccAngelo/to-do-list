5556fc00db6d1c1d76d8f7186157efe4b93a904f
micccAngelo <michaelangelo0109@gmail.com>
2023-02-22 14:21:16 +0700

    third

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,44 +1,41 @@
 import React from 'react';
-const ToDo = ({ toDo, doneTask, deleteTask }) => {
-  // Keep track of the maximum task id
-  const maxTaskId = toDo.reduce((maxId, task) => Math.max(maxId, task.id), 0);
+const ToDo = (props) => {
 
   return (
     <>
-      {toDo &&
-        toDo
+      {props.toDo &&
+        props.toDo
+        //untuk data jika dimasukan manual
           .sort((a, b) => a.id > b.id ? 1 : -1)
-          .map((task) => {
+          .map((task, index) => {
             return (
-              <React.Fragment key={task.id}>
-                <div className="col taskBG">
-                  <div className={task.status ? "done" : ""}>
-                    <div className="positionNo">
-                      <span className="taskNo">
-                        {task.id - (maxTaskId - toDo.length)}
-                      </span>
-                      <span className="taskText">{task.title}</span>
-                    </div>
-                    <div className="buttonWrap">
-                      <span
-                        title={task.status ? "Undone" : "Done"}
-                        onClick={() => doneTask(task.id)}
+              <div className="col taskBG" key={task.id}>
+                <div className={task.status ? "done" : ""}>
+                  <div className="positionNo">
+                    <span className="taskNo">
+                      {index + 1}
+                    </span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="buttonWrap">
+                    <span
+                      title={task.status ? "Undone" : "Done"}
+                      onClick={() => props.doneTask(task.id)}
+                    >
+                      <button
+                        className={task.status ? "btnUndone" : "btnDone"}
                       >
-                        <button
-                          className={task.status ? "btnUndone" : "btnDone"}
-                        >
-                          {task.status ? "Undone" : "Done"}
-                        </button>
-                      </span>
-                      <span title="Delete" onClick={() => deleteTask(task.id)}>
-                        <button className="btnDelete">Delete</button>
-                      </span>
-                    </div>
+                        {task.status ? "Undone" : "Done"}
+                      </button>
+                    </span>
+                    <span title="Delete" onClick={() => props.deleteTask(task.id)}>
+                      <button className="btnDelete">Delete</button>
+                    </span>
                   </div>
                 </div>
-              </React.Fragment>
-            );
-          })}
+              </div>
+          );
+        })}
     </>
   );
 };
